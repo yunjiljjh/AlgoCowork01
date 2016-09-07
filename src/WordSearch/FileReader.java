e5da29fc189b543f5abf176446be77a4604286bc
yunjiljjh <yunjiljjh@naver.com>
2016-09-07 23:56:13 +0900

    first
    first

---
+++ src/WordSearch/FileReader.java
@@ -0,0 +1,14 @@
+package WordSearch;
+
+import java.io.BufferedReader;
+//import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class FileReader {
+	private FileReader fr;
+	private BufferedReader br;
+
+}
---
+++ src/WordSearch/Solve.java
@@ -0,0 +1,5 @@
+package WordSearch;
+
+public class Solve {
+
+}
