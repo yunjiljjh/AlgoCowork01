1ef4fdd51448d9e3a7ab665768ae7e0ccd692c26
yunjiljjh <yunjiljjh@naver.com>
2016-09-08 00:58:27 +0900

    .
    .

--- src/WordSearch/Solve.java
+++ src/WordSearch/Solve.java
@@ -1,14 +1,24 @@
 package WordSearch;
 
 public class Solve {
-	private int [][] dictionary;
-	private int [][] words;
-	private int [][] alphabet;
-	private int [][] currentPoint;
-	private int [][] indicatingPoint;
+	private int [][] dictionary; // tile array given
+	private int [][] words; // words you have to search
+	private int [][] alphabet; // from a-z location of each alphabet in the dicationary[][]
+	private int currentPointX; //current location of 'x' in (x,y) of dictionary[][]
+	private int currentPointY; //current location of 'y' in (x,y) of dictionary[][]
+	private int indicatingPointX;  //current location of 'x' in (x,y) of words[][]
+	private int indicatingPointY;  //current location of 'y' in (x,y) of words[][]
+
 	private int [][] startingPoint;
 	private	int [][] endingPoint;
-
-		public Solve(){
+	
+	    public Solve(){
+	    	
+	    }
+		public Solve(int[][] dictionary, int[][] words, int[][] alphabet){
+			this.dictionary = dictionary;
+			this.words = words;
+			this.alphabet = alphabet;
 		}
+		
 }
