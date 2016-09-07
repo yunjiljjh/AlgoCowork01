fbfb300112e3880bd96f3984f6ca2361ce9b3052
yunjiljjh <yunjiljjh@naver.com>
2016-09-08 00:06:50 +0900

    .
    .

--- src/WordSearch/FileReader.java
+++ src/WordSearch/FileReader.java
@@ -1,14 +1,32 @@
 package WordSearch;
 
-import java.io.BufferedReader;
-//import java.io.FileReader;
+import java.io.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+
 public class FileReader {
 	private FileReader fr;
 	private BufferedReader br;
+	
+	private int pointer;
+	
+	public FileReader(String path){
+		pointer=0;
+		read(path);
+	}
+	private void read(String path){
+		try{
+		fr=new FileReader(path);
+		br=new BufferedReader(fr);
 
+	
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
