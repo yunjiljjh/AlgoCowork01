a36d1f89bd2072bfdfab37f488419e8f7197b0c7
dasomJ <sommy322odd@gmail.com>
2016-09-08 10:35:40 +0900

    Add files via upload

--- src/WordSearch/InputReader.java
+++ src/WordSearch/InputReader.java
@@ -13,17 +13,52 @@ package WordSearch;
 		
 		private int pointer;
 		
+		public int n;
+		public int m;
+		public char dictionary[][];
+		public char words[][];
+		
 		public InputReader(String path){
 			pointer=0;
 			read(path);
+		
 		}
 		private void read(String path){
 			try{
 			fr=new FileReader(path);
 			br=new BufferedReader(fr);
-		
+			
+			String s = null;
+			
+			s = br.readLine();
+			char[] firstLine = s.split();
+			n = Integer.parseInt(firstLine[0]);
+			m = Integer.parseInt(firstLine[1]);
+			
+			while((s=br.readLine())!=null){
+				while (s!==""){
+					for (i=0; i<s.length(); i++){
+						dictionary[i] = s;
+					}
+				}
+				
+				
+			}
 			}catch(IOException e){
 				e.printStackTrace();
+			}finally{
+				if(br != null){
+					try{
+						br.close();
+					}catch(IOException e){
+					}
+				}
+				if(fr != null){
+					try{
+						fr.close();
+					}catch(IOException e){
+					}
+				}
 			}
 		}
 		
