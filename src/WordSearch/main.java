c438298124b73bf9f7949f7469c28c5ce1700310
yunjiljjh <yunjiljjh@naver.com>
2016-09-08 20:05:06 +0900

    .
    .

--- src/WordSearch/main.java
+++ src/WordSearch/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		InputReader reader=new InputReader("C:\\hw1\\input.txt");
+		Solve s = new Solve(reader.dictionary, reader.words, reader.N,reader.M);
 	}
 
 }
