799e8f69aecb44cb4f9d8134362d2726cad2a141
dasomJ <sommy322odd@gmail.com>
2016-09-08 23:26:35 +0900

    Add files via upload

--- src/WordSearch/InputReader.java
+++ src/WordSearch/InputReader.java
@@ -20,63 +20,64 @@ package WordSearch;
 		
 		private void read(String path){
 			try{
-			fr=new FileReader(path);
-			br=new BufferedReader(fr);
+				fr=new FileReader(path);
+				br=new BufferedReader(fr);
 			
-			String s = null;
-			int rowDic = 0;
-			int rowWord = 0;
+				String s = null;
+				int rowDic = 0;
+				int rowWord = 0;
 			
-			// reads the first line and set N, M
-			s = br.readLine();
-			String[] a = s.split(" ");
-			N = Integer.parseInt(a[0]);
-			M = Integer.parseInt(a[1]);
+				// reads the first line and set N, M
+				s = br.readLine();
+				String[] a = s.split(" ");
+				N = Integer.parseInt(a[0]);
+				M = Integer.parseInt(a[1]);
 			
-			dictionary = new char[N][N];
-			words = new char[M][];
+				dictionary = new char[N][N];
+				words = new char[M][];
 			
-			/* set dictionary[][]. Remove whitespaces. Put each character of the lines to dictionary. 
-			 * rowDic is pointer.
-			 */
-			while((s=br.readLine())!=null){
-				String[] ss = s.split(" ");
-				char[] c = new char[ss.length];
-				for (int i=0; i<N; i++){
-					c[i] = ss[i].charAt(0);
+				/* set dictionary[][]. Remove whitespaces. Put each character of the lines to dictionary. 
+				 * rowDic is pointer.
+				 */
+				while((s=br.readLine())!=null){
+					String[] ss = s.split(" ");
+					char[] c = new char[ss.length];
+					for (int i=0; i<N; i++){
+						c[i] = ss[i].charAt(0);
+					}
+					for (int i=0; i<N; i++){
+						dictionary[rowDic][i] = c[i];
+					}
+					rowDic++;
+					if (rowDic == N) break;
 				}
-				for (int i=0; i<N; i++){
-					dictionary[rowDic][i] = c[i];
+				while((s=br.readLine())!=null){
+					if (rowWord==M-1) break;
+					char[] c = s.toCharArray();
+					words[rowWord] = new char[c.length];
+					for (int j=0; j<c.length; j++){
+						words[rowWord][j] = c[j];
+					}
+					rowWord++;
 				}
-				rowDic++;
-				if (rowDic == N) break;
-			}
-			while((s=br.readLine())!=null){
-				if (rowWord==M-1) break;
-				char[] c = s.toCharArray();
-				words[rowWord] = new char[c.length];
-				for (int j=0; j<c.length; j++){
-					words[rowWord][j] = c[j];
-				}
-				rowWord++;
-			}
 			}catch(IOException e){
 				e.printStackTrace();
 			}finally{
 				if(br != null){
-					try{
-						br.close();
-					}catch(IOException e){
-					}
+					try{br.close();}
+					catch(IOException e){}
 				}
 				if(fr != null){
-					try{
-						fr.close();
-					}catch(IOException e){
-					}
+					try{fr.close();}
+					catch(IOException e){}
 				}
 			}
 		}
+		
+		public int getN(){return this.N;}
+		public int getM(){return this.M;}
+		public char[][] getDic(){return this.dictionary;}
+		public char[][] getWords(){return this.words;}
 		
 		
 }
